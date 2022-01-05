80c1b59fea08f9f8ef8d96b44880d6b8a270c511
BlessOE <93702286+BlessOE@users.noreply.github.com>
2022-01-05 22:42:27 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,6 @@
 $(document).ready(function() {
     //start
 
-    // // layered nav selection
-    // $("#showMoreCourses").click(function(){
-    //     console.info("I was clicked Courses!");
-    //     $("#subMenuCourses").slideToggle();
-    // });
-    // $("#showMoreInfo").click(function(){
-    //     console.info("I was clicked Info!");
-    //     $("#subMenuInfo").slideToggle();
-    // });
-    
     // burger menu
     $(".burger").click(function(){
         console.info("I was clicked Burger Menu!");
@@ -19,15 +9,10 @@ $(document).ready(function() {
 
     // image slider
     $("#lightSlider").lightSlider({
-            // gallery: true,
             item: 1,
             loop:true,
             slideMargin: 0,
     });
     
-    $(".submitContainer").click(function() {
-        $(".popup").fadeIn();
-        // $("#overlay").show();
-    })
     //end
 })
